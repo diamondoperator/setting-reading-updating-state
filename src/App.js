f7d998a1c438159c2152b56c52ea9472c50e45ee
Raul Barrera <rbarrera1992@gmail.com>
2020-09-30 11:15:54 -0500

    updated

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function App() {
+  return (
+    <main className='App'>
+      {/* content goes here */}
+    </main>
+  );
+}
+
+export default App;
